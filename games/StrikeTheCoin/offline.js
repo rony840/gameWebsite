0509d69e7598baad705a0f1dd316203083622974
Ronit Fozdar <ronitfozdar99@gmail.com>
2024-10-20 12:07:45 -0400

    gameSite upload

---
+++ games/StrikeTheCoin/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1623054805,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/button_info-sheet0.png",
+		"images/button_play-sheet0.png",
+		"images/p11-sheet0.png",
+		"images/table-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
---
+++ games/TankCombat/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1607684072,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/s_background-sheet0.png",
+		"images/s_tank-sheet0.png",
+		"images/s_tank-sheet1.png",
+		"images/s_bullet-sheet0.png",
+		"images/obstacle-sheet0.png",
+		"images/gamepad_middle-sheet0.png",
+		"images/gamepad-sheet0.png",
+		"images/fire_button-sheet0.png",
+		"images/splashscreen-sheet0.png",
+		"images/black1080p-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
